416e1b69167da813fa5dabab9eb2cd38f9447198
FacePluslll <34409891+FacePluslll@users.noreply.github.com>
2021-02-16 20:14:31 -0600

    adding final changes

---
+++ README.md
@@ -0,0 +1,2 @@
+# Infix evaluation program
+This program will take an input file and turn out an N amount of answers from the file.
--- src/main/infixInput.java
+++ src/main/infixInput.java
@@ -1,3 +1,5 @@
+package src.main;
+
 import java.util.Scanner;
 import java.util.Stack;
 import java.io.*;
@@ -9,7 +11,7 @@ import java.util.Queue;
 public class infixInput {
 	public static boolean isBoolExpression = false;
 	public static void main(String[] args ) throws FileNotFoundException{
-		FileInputStream inputFile = new FileInputStream("input.txt");
+		FileInputStream inputFile = new FileInputStream("src/input.txt");
 		Scanner scnr = new Scanner(inputFile);
 		
 
